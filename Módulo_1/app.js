b70d63359a5e61bebdbfecfd2616f64336aae659
Phillip Marques <phillip.silva@capgemini.com>
2022-06-17 18:22:26 -0300

    Criando Objetos

--- Módulo_1/app.js
+++ Módulo_1/app.js
@@ -1,1 +1,29 @@
-console.log('Hello World')
+var carro = {}
+
+carro.velocidade = 0
+carro.ligado = false
+carro.nivelCombustivel = 100
+carro.quantPortas = 4
+carro.combustivel = "flex"
+carro.cor = "Vermelho"
+carro.potencia = 100
+
+carro.ligar = function() {
+    if (this.ligado) {
+        console.log("Carro já ligado")
+    } else {
+        this.ligado = true
+    }
+}
+
+carro.andar = function() {
+    if (!this.ligado) {
+        this.ligar()
+    }
+    this.velocidade = 40
+
+}
+
+carro.andar()
+console.log(carro.velocidade)
+console.log(carro.ligado)
