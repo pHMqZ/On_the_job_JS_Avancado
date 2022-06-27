be4130759464eab7831f209f3506860b6d352e54
Phillip Marques <phillip.silva@capgemini.com>
2022-06-27 17:37:40 -0300

    Arrow Functions

---
+++ Módulo_3/arrowFunc.js
@@ -0,0 +1,28 @@
+//único parâmetro
+const adicionar2 = a => a + 2
+console.log(adicionar2(1))
+
+//Multiplos parâmetros
+const adicionar = (a, b) => a + b
+console.log(adicionar(1, 1))
+
+//Sem parâmetros
+const return2 = () => 2
+console.log(return2())
+
+//declarações e expressões
+const retornaA = () => "a"
+console.log(retornaA())
+
+const retorna1mais1 = () => {
+    let a = 1
+    let b = 1
+    return a + b
+}
+console.log(retorna1mais1())
+
+const voidFunction = () => {
+    console.log("dentro da void")
+}
+const voidArrow = () => voidFunction()
+console.log(voidArrow())
---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>JavaScript Avançado</h1>
+
+<p>
+    Repositório com exercícios de JavaScript Avançado do treinamento On the Job;</br>
+    Abrangendo JS Modular, JS Funcional e Interface de PRogramação de Apps
+</p>
+
+
