7d8327cb559764b655f89af997cee92502878aef
Phillip Marques <phillip.silva@capgemini.com>
2022-06-27 15:36:08 -0300

    Transparência Referencial

---
+++ Módulo_3/transRef.js
@@ -0,0 +1,15 @@
+function soma(a, b) {
+    return a + b
+}
+
+function retorna2() {
+    return 2
+}
+
+//substitui retorna2 por 2
+console.log(soma(2, retorna2()))
+console.log(soma(2, 2))
+
+//substitui soma(1,1) por 2
+console.log(soma(1, 1) + 1)
+console.log(2 + 1)
