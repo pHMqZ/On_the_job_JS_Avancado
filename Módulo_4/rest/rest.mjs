f909ecf3b540a5942327e34685f61c4ad321aad2
Phillip Marques <phillip.silva@capgemini.com>
2022-07-04 10:48:17 -0300

    Rest final

--- Módulo_4/rest/rest.mjs
+++ Módulo_4/rest/rest.mjs
@@ -5,12 +5,45 @@ const apiCall = (endpoint, method, data) => {
     return fetch(uri, {
             method: method,
             headers: {
-                "Content-type": "application/json"
+                "Content-type": "application/json;"
             },
             body: data ? data : null
         })
         .then(response => response.json())
         .then(json => json)
+        .catch(error => error)
 }
 
-apiCall("posts", "GET").then(console.log)
+const apiGet = async(resource, id) => {
+    let endpoint = id ? `${resource}/${id}` : resource
+    console.log(await apiCall(endpoint, "GET"))
+}
+
+const apiPost = async(resource, data) => {
+    let endpoint = resource
+    console.log(await apiCall(endpoint, "POST", data))
+}
+
+const apiPut = async(resource, id, data) => {
+    let endpoint = `${resource}/${id}`
+    console.log(await apiCall(endpoint, "PUT", data))
+}
+
+const apiDelete = async(resource, id) => {
+    let endpoint = `${resource}/${id}`
+    console.log(await apiCall(endpoint, "DELETE"))
+}
+
+apiGet("posts")
+apiGet("posts", 42)
+apiPost("post", {
+    "userID": 123,
+    "title": "teste",
+    "body": "Corpo teste"
+})
+apiPut("post", 44, {
+    "userID": 123,
+    "title": "teste",
+    "body": "Corpo teste"
+})
+apiDelete("post", 44)
